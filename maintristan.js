5296cbf4b923c6ff7f6a9b5ae5ad9c5809fdc1be
3ptstris <128217016+3ptstris@users.noreply.github.com>
2023-05-31 17:40:01 +0200

    Add files via upload

---
+++ maintristan.js
@@ -0,0 +1,16 @@
+function findTwoSumIndices(nums, target) {
+    const indices = new Map();
+  
+    for (let i = 0; i < nums.length; i++) {
+      const complement = target - nums[i];
+  
+      if (indices.has(complement)) {
+        return [indices.get(complement), i];
+      }
+  
+      indices.set(nums[i], i);
+    }
+  
+    return [];
+  }
+  
